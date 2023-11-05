7d719df53a85d3cebada6a9a6c49fcf3c1e98e77
aleksandr.stolbov <aleksandr.stolbov@libertexgroup.com>
2023-11-05 16:11:23 +0100

    add axios, update signin logic, notification and initial error handling

--- ui/src/views/signup.js
+++ ui/src/views/signup.js
@@ -1,10 +1,47 @@
-import { Row, Col, Button, Form, Input } from "antd";
+import { Row, Col, Button, Form, Input, notification } from "antd";
 import { LockOutlined, UserOutlined, MailOutlined } from '@ant-design/icons';
-import React from "react";
+import React, {useState} from "react";
+import axios  from 'axios';
 
 const SignupView = () => {
+    const [form] = Form.useForm()
+    const [api, contextHolder] = notification.useNotification()
+    const [isLoading, setLoading] = useState(false)
+
+    const openNotification = (type, t, d) => {
+        if (t.length === 0) t = "General error";
+
+        api[type]({
+            message: t,
+            description: d
+        })
+    }
+
+    const onFinish = (vals) => {
+        axios.post('http://localhost:8080/auth/register', {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            data: {
+                email: vals.email,
+                name: vals.name,
+                password: vals.password,
+            }
+        }).then((resp) => {
+            console.log(resp)
+        }).catch((err) => {
+            openNotification('error', '', err.message)
+            console.log(err)
+        })
+
+
+
+        setLoading(true);
+    }
+
     return (
         <Row  justify="center" style={styles.container}>
+            {contextHolder}
             <Col span={18}>
                 <Row>
                     <Col span={10} style={styles.left}>
@@ -13,21 +50,43 @@ const SignupView = () => {
                     <Col span={14} style={styles.right}>
                         <h1 style={styles.h}>Create Account</h1>
 
-                        <Form name="login">
+                        <Form name="login"
+                              form={form}
+                              autoComplete="off"
+                              onFinish={onFinish}
+                              disabled={isLoading} >
+
                             <Form.Item name="username" rules={[{ required: true, message: "Input your username" }]}>
                                 <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
                             </Form.Item>
+
                             <Form.Item name="email" rules={[{ required: true, message: "Input your email" }]}>
                                 <Input prefix={<MailOutlined />} placeholder="Email" />
                             </Form.Item>
+
                             <Form.Item name="password" rules={[{ required: true, message: "Input your password" }]}>
                                 <Input prefix={<LockOutlined className="site-form-item-icon" />} type="password" placeholder="Password" />
                             </Form.Item>
-                            <Form.Item name="password2" rules={[{ required: true, message: "Input your password" }]}>
+
+                            <Form.Item name="password2"
+                                       rules={[
+                                           {
+                                               required: true,
+                                               message: "Must be the same password"
+                                           }
+                                       ]}>
                                 <Input prefix={<LockOutlined className="site-form-item-icon" />} type="password" placeholder="Password again" />
                             </Form.Item>
+
                             <Form.Item>
-                                <Button type="primary" shape="round" size="large" htmlType="submit" className="login-form-button" block >Register</Button><br/>
+                                <Button type="primary"
+                                        shape="round"
+                                        size="large"
+                                        htmlType="submit"
+                                        className="login-form-button"
+                                        block
+                                        loading={isLoading}
+                                >Register</Button><br/>
                                 
                             </Form.Item>
                             <Form.Item>
@@ -54,7 +113,6 @@ const styles = {
     },
     left: {
         minHeight: "560px",
-        background: "#1677ff",
         borderRadius: "10px 0 0 10px",
         padding: "50px 20px",
         background: "#fff url(sign-up.png) no-repeat center center",
