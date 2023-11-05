f6c28fa7e5ce5b821fe1eea11c237f897528ed16
aleksandr.stolbov <aleksandr.stolbov@libertexgroup.com>
2023-11-05 17:57:24 +0100

    Sign in component implementation

--- ui/src/views/signin.js
+++ ui/src/views/signin.js
@@ -1,10 +1,61 @@
-import React from "react";
-import { Row, Col, Button, Form, Input } from "antd";
+import React, {useState} from "react";
+import { useNavigate } from "react-router-dom";
+import {Row, Col, Button, Form, Input, notification} from "antd";
 import { LockOutlined, MailOutlined } from '@ant-design/icons';
+import axios from "axios";
 
-const SigninView = () => {
+const SigninView = (props) => {
+    const [form] = Form.useForm();
+    const [api, contextHolder] = notification.useNotification()
+    const [isLoading, setLoading] = useState(false)
+    const navigate = useNavigate();
+
+    const openNotification = (type, t = "", d) => {
+        if (t.length === 0) t = "General error";
+
+        api[type]({
+            message: t,
+            description: d
+        })
+    }
+
+    const onFinish = (v) => {
+        setLoading(true)
+
+        let data = {
+            email: v.email,
+            password: v.pass
+        }
+
+        let opts = {
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }
+
+        axios.post("http://localhost:8080/auth/signin", data, opts)
+            .then((resp) => {
+                console.log(resp)
+                if (resp.status === 201) {
+                    localStorage.setItem("token", resp.data.token)
+                    navigate("/")
+                }
+
+            })
+            .catch((err) => {
+                openNotification('error', err.code, err.message)
+                form.setFieldValue("password", "")
+                setLoading(false)
+                console.log('err')
+                console.log(err)
+            })
+
+    }
+
     return (
         <Row  justify="center" style={styles.container}>
+            {contextHolder}
+
             <Col span={18}>
                 <Row>
                     <Col span={10} style={styles.left}>
@@ -13,15 +64,31 @@ const SigninView = () => {
                     <Col span={14} style={styles.right}>
                         <h1 style={styles.h}>Sign In</h1>
 
-                        <Form name="login">
+                        <Form name="login"
+                              autoComplete="off"
+                              onFinish={onFinish}
+                              disabled={isLoading}
+                              form={form} >
+                            {/*<Alert message="Try modify " type="info" showIcon />*/}
+
                             <Form.Item name="email" rules={[{ required: true, message: "Input your email" }]}>
                                 <Input prefix={<MailOutlined />} placeholder="Email" />
                             </Form.Item>
-                            <Form.Item name="password2" rules={[{ required: true, message: "Input your password" }]}>
+
+                            <Form.Item name="pass"
+                                       rules={[{ required: true, message: "Input your password" }]}>
                                 <Input prefix={<LockOutlined className="site-form-item-icon" />} type="password" placeholder="Password again" />
                             </Form.Item>
+
                             <Form.Item>
-                                <Button type="primary" shape="round" size="large" htmlType="submit" className="login-form-button" block >Login</Button><br/>
+                                <Button type="primary"
+                                        shape="round"
+                                        size="large"
+                                        htmlType="submit"
+                                        className="login-form-button"
+                                        block
+                                        loading={isLoading}
+                                >Login</Button><br/>
                                 
                             </Form.Item>
                             <Form.Item>
